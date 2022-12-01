7f933913c889c45ae82a8bf9f0a1e421d7388b37
xiongjiazheng <eilfa@bu.edu>
2022-12-01 15:49:16 -0500

    pa4

--- src/java/simpledb/JoinOptimizer.java
+++ src/java/simpledb/JoinOptimizer.java
@@ -111,7 +111,8 @@ public class JoinOptimizer {
             // HINT: You may need to use the variable "j" if you implemented
             // a join algorithm that's more complicated than a basic
             // nested-loops join.
-            return -1.0;
+            //return -1.0;
+            return cost1 + card1 * cost2 + card1 * card2;
         }
     }
 
@@ -155,9 +156,16 @@ public class JoinOptimizer {
             String field2PureName, int card1, int card2, boolean t1pkey,
             boolean t2pkey, Map<String, TableStats> stats,
             Map<String, Integer> tableAliasToId) {
-        int card = 1;
+        //int card = 1;
         // some code goes here
-        return card <= 0 ? 1 : card;
+        //return card <= 0 ? 1 : card;
+        if (joinOp == Predicate.Op.EQUALS) {
+            if(t1pkey||t2pkey) return t1pkey?card2:card1;
+
+            return Math.max(card1, card2);
+        }
+        return (int)(0.3*card1*card2);
+
     }
 
     /**
@@ -221,7 +229,32 @@ public class JoinOptimizer {
 
         // some code goes here
         //Replace the following
-        return joins;
+        //return joins;
+        PlanCache planCache = new PlanCache();
+        Set<LogicalJoinNode> joinSet = null;
+
+        for (int i = 0; i < joins.size(); i++) {
+            Set<Set<LogicalJoinNode>> setSet = this.enumerateSubsets(this.joins, i + 1);
+            for (Set<LogicalJoinNode> s : setSet) {
+                if (s.size() == joins.size())
+                    joinSet = s;
+
+                double bestCost = Double.MAX_VALUE;
+                CostCard bestPlan = new CostCard();
+                for (LogicalJoinNode toRemove : s) {
+                    CostCard plan = computeCostAndCardOfSubplan(stats, filterSelectivities, toRemove, s, bestCost, planCache);
+                    if (plan != null) {
+                        bestPlan = plan;
+                        bestCost = plan.cost;
+                    }
+                }
+                if (bestPlan.plan != null)
+                    planCache.addPlan(s, bestPlan.cost, bestPlan.card, bestPlan.plan);
+            }
+        }
+
+
+        return planCache.getOrder(joinSet);
     }
 
     // ===================== Private Methods =================================
